6f4d37ced00fed7638da05b546a83e834f0c8498
Chandler Casey <chandlercasey13@gmail.com>
2024-07-10 15:27:49 -0500

    added chatbox component and removed vite fluff

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react';
+
+
+import ChatBox from './components/ChatBox';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      
+      <ChatBox/>
+
     </>
   )
 }
---
+++ src/components/ChatBox.jsx
@@ -0,0 +1,13 @@
+
+
+function ChatBox () {
+
+
+return <h1>yo</h1>
+
+}
+
+
+  
+  export default ChatBox;
+  
