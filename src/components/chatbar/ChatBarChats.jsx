36c859494ec92bcb44b4f456e6c2a5510770f59e
cbobak671 <cbobak.671@gmail.com>
2024-07-15 08:04:08 -0700

    Update ChatBarChats

--- src/components/chatbar/ChatBarChats.jsx
+++ src/components/chatbar/ChatBarChats.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from "react";
+
 const ChatBarChats = () => {
+  const useGetChats = () => {
+    const [ loading, setLoading ] = useState(false);
+    const [ chats, setChats ] = useState([]);
+
+    useEffect(() => {
+      const getChats = async () => {
+        try {
+          const res = await fetch("/users");
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      getChats();
+    }, []);
+    return { loading, chats };
+  };
+
   return (
     <>
-      <div className="insert inline styling here">
+      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
         <div className="avatar online here">
-          <div className="insert inline styling here">
+          <div className="w-10 rouned-full">
             <img src="img" alt="avatar" />
           </div>
         </div>
-        <div className="inline styling here">
-          <div className="inline styling here">
-            <p className="inline styling here"></p>
-            <span className="text-xl">emoji here</span>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-100"></p>
+            <span className="text-l">Chatter User</span>
           </div>
         </div>
       </div>
