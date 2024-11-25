c9a42e24aef88514f64e79281d9fdba9bbceadd5
Chandler Casey <chandlercasey13@gmail.com>
2024-11-25 06:18:00 -0600

    Make login container mobile responsive

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
 
 const URL = "https://chatter-messenger-app-server-07eacc0c4ebf.herokuapp.com"
-console.log(process.env.NODE_ENV)
+
 
 export const socket = io(URL);
