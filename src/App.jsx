0412dbc1c0ebc195cbad3732314e541c4ed8ac5f
Chandler Casey <chandlercasey13@gmail.com>
2024-11-29 15:57:25 -0600

    Fix url issue when refresh

--- src/App.jsx
+++ src/App.jsx
@@ -64,19 +64,19 @@ const handleOpenSidebar =function () {
   }, [previewMessage, user]);
  
 
-  useEffect(()=>{
+  // useEffect(()=>{
     
-    if (userChats.length>0 && !isInChat && user){
-      navigate(`/chatlogs/${userChats[0][0]?._id}/user/${userChats[0][0]?.participants[0].username === user.username ? `${userChats[0][0]?.participants[1]._id}`: `${userChats[0][0]?.participants[0]._id}`}/${userChats[0][0]?.participants[0]?.username === user.username ? `${userChats[0][0]?.participants[1]?.username}`: `${userChats[0][0]?.participants[0].username}` }
-        ` );
-    } else if (userChats.length === 0 && user) {
-      navigate(`/chatlogs/undefined/user/${user._id}/${user.username} ` );
-    }
+  //   if (userChats.length>0 && !isInChat && user){
+  //     navigate(`/chatlogs/${userChats[0][0]?._id}/user/${userChats[0][0]?.participants[0].username === user.username ? `${userChats[0][0]?.participants[1]._id}`: `${userChats[0][0]?.participants[0]._id}`}/${userChats[0][0]?.participants[0]?.username === user.username ? `${userChats[0][0]?.participants[1]?.username}`: `${userChats[0][0]?.participants[0].username}` }
+  //       ` );
+  //   } else if (userChats.length === 0 && user) {
+  //     navigate(`/chatlogs/undefined/user/${user._id}/${user.username} ` );
+  //   }
     
       
     
     
-    },[userChats])
+  //   },[userChats])
 
   
 
